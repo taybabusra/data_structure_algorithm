4de532e1904d1dd8e49d20a9f35de20d6135c272
Tayba Busra <taybabusra@gmail.com>
2022-09-06 20:31:36 +0600

    Update tree_bst_or_not.cpp

--- tree/binary_search_tree/tree_bst_or_not.cpp
+++ tree/binary_search_tree/tree_bst_or_not.cpp
@@ -49,3 +49,32 @@ int main(){
        cout<<"The tree is not BST"<<endl;
     return 0;
 }
+// https://practice.geeksforgeeks.org/problems/check-for-bst/1?page=1&category[]=Tree&curated[]=1&sortBy=submissions
+
+class Solution
+{
+    public:
+    //Function to check whether a Binary Tree is BST or not.
+    bool isBST(struct Node* node)
+    {
+      return(isBSTUtil(node, INT_MIN, INT_MAX));
+    }
+    
+    public:
+    int isBSTUtil(struct Node* node, int min, int max)
+    {
+        //an empty tree is BST so we return true.
+        if (node==NULL)
+         return 1;
+        
+        //returning false if this node violates the min/max constraint.
+        if (node->data < min || node->data > max)
+         return 0;
+        
+        //otherwise checking the subtrees recursively.
+        //tightening the min or max constraint.
+        return
+            isBSTUtil(node->left, min, node->data-1) &&  
+            isBSTUtil(node->right, node->data+1, max); 
+    }
+};
